1b88d2f2a0122c8208e52217ef2d419c3ed3de44
Harshinielangovan57 <harshinielangovan57@gmail.com>
2025-10-15 20:15:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { SinglePageCv } from './Pages/SinglePageCv';
+
+function App() {
+  return (
+    <div>
+      <SinglePageCv />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/SinglePageCv.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const SinglePageCv = () => {
+  return (
+    <div className='header'>
+        <hr />
+        <h2>Harshini Elangovan</h2>
+        <h3>Frontend Developer</h3>
+        <p>23/56 KV street<br />kalpatty,Manapparai(Tk)<br/>Trichy-621315</p>
+        <h3>Skills</h3>
+        <p>Html, CSS, React, MongoDb, Node.js, JavaScript, python</p>
+        <h3>Education</h3>
+        <p1>Sakthi sai matric hr sec school - Dindigul</p1>
+        <p>2019-2022<br/>class: 12 <br/>percentage : 12</p>
+        <p1>SSMIET - Dindigul</p1>
+        <p>2022-2026<br/>B.Tech AI&DS<br/>CGPA : 8.5</p>
+        <h3>Internship</h3>
+        <p><strong>MentorBridge<br/>Frontend Developer<br/>Technology used : HTML, CSS, Js And React</strong></p>
+        <ul>
+            <li>I did a Frontend Development Internship at MentorBridge, where I worked on building and improving user interfaces for web applications.</li>
+            <li>During the internship, I learned to use technologies like React.js, JavaScript and Bootstrap to create responsive designs.</li>
+            <li>I also gained experience in handling real-world projects, working in a team environment, fixing bugs, and improving performance.</li>
+            <li>This internship helped me understand how frontend development works in a professional setting and improved my problem-solving and coding skills.</li>
+        </ul>
+    </div>
+  )
+}
